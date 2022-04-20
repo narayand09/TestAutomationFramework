f189e85d956091d6aaeaa677801798ebec962cfd
Narayan Datt <narayandatt@promantus.com>
2022-04-21 04:25:06 +0530

    addec cartpage

---
+++ src/main/java/com/qa/opencart/pages/Cartpage.java
@@ -0,0 +1,11 @@
+package com.qa.opencart.pages;
+
+public class Cartpage {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i = 10;
+		System.out.println(i);
+	}
+
+}
