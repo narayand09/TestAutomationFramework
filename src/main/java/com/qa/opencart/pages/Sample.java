5cacfc543048b898f895a9f34c08a860996955de
Narayan Datt <narayandatt@promantus.com>
2022-04-22 20:10:35 +0530

    Narayan's code

---
+++ src/main/java/com/qa/opencart/pages/Sample.java
@@ -0,0 +1,14 @@
+package com.qa.opencart.pages;
+
+public class Sample {
+
+	
+	public void login() {
+		String user="Narayan";
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
