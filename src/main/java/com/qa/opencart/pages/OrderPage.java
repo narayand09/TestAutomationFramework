c4402a55a2a6530d2d84c3140ad57aeaf2b30afd
narayand09 <53884059+narayand09@users.noreply.github.com>
2022-04-22 20:23:46 +0530

    Create OrderPage.java

---
+++ src/main/java/com/qa/opencart/pages/OrderPage.java
@@ -0,0 +1,14 @@
+package com.qa.opencart.pages;
+
+public class OrderPage {
+
+	public static void main(String[] args) {
+		
+
+	}
+	
+	public void makeOrder() {
+		System.out.println("Order Page ");
+	}
+
+}
