ed41adb542ff4abba4c644232e57bd5227ab8328
narayand09 <53884059+narayand09@users.noreply.github.com>
2022-04-22 20:08:06 +0530

    Create Sample.java

---
+++ src/main/java/com/qa/opencart/pages/Sample.java
@@ -0,0 +1,15 @@
+package com.qa.opencart.pages;
+
+public class Sample {
+
+	
+	public void login() {
+		String user="Tom";
+    String password="Tom123";
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
