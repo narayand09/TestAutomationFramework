f36c1209043a87d67351d5f7b0732e16d2ff5429
Narayan Datt <narayandatt@promantus.com>
2022-04-24 10:20:23 +0530

    updated login page

--- src/main/java/com/qa/opencart/pages/LoginPage.java
+++ src/main/java/com/qa/opencart/pages/LoginPage.java
@@ -22,6 +22,7 @@ public class LoginPage {
 	private By loginBtn=By.xpath("//input[@value='Login']");
 	private By forgotPwd=By.linkText("Forgotten Password");
 	private By registerLink=By.linkText("Register");
+	private By Logout=By.linkText("Logout");
 	private By loginErrorMessg = By.cssSelector("div.alert.alert-danger.alert-dismissible");
 	
 	// 2. Public Page Constructor
